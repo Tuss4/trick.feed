8d843ad9483faa2ac8456c29c456832ab97cc6ba
Tuss4 <tuss4dbfn@gmail.com>
2014-05-26 13:05:36 -0500

    Made some modifications to the model schema, playing with fetches in data.py

--- trickfeed/models.py
+++ trickfeed/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Tricker(models.Model):
     user = models.OneToOneField(User)
+    favorites = models.ManyToManyField('Video')
 
     def __unicode__(self):
         return self.user.username
@@ -12,9 +13,9 @@ class Tricker(models.Model):
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    youtube_url = models.CharField(max_length=50)
+    youtube_id = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    author_url = models.CharField(max_length=100)
+    author_id = models.CharField(max_length=100)
 
     def __unicode__(self):
         return "%s - %s".format(self.title, self.youtube_url)
