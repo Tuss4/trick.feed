ada9165ef31bb398806986129582daabed0fcdb3
Tuss4 <tuss4dbfn@gmail.com>
2014-06-13 00:21:21 -0500

    Did some light linting before sleep. Time for sleep.

--- trickfeed/forms.py
+++ trickfeed/forms.py
@@ -7,10 +7,10 @@ class LoginForm(forms.Form):
                                widget=forms.TextInput(
                                    attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(
-                                   attrs={'class': 'form-control'}))
+                               attrs={'class': 'form-control'}))
 
 
 class RegistrationForm(LoginForm):
     email = forms.EmailField(widget=forms.EmailInput(
-                                 attrs={'class': 'form-control'}))
+                             attrs={'class': 'form-control'}))
     captcha = CaptchaField()
--- trickfeed/models.py
+++ trickfeed/models.py
@@ -3,14 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Tricker(models.Model):
+    '''The tricker/member class.
+    Extends the default User class
+    via a OneToOneField.'''
+
     user = models.OneToOneField(User)
     favorites = models.ManyToManyField('Video')
 
+    # Default string representation for a
+    # tricker instance.
     def __unicode__(self):
         return self.user.username
 
 
 class Video(models.Model):
+    '''The video class. Represents the data
+    scraped from the YouTube API.'''
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     youtube_id = models.CharField(max_length=50)
@@ -20,15 +29,25 @@ class Video(models.Model):
     thumbnail = models.CharField(max_length=100)
     is_tricking = models.NullBooleanField(default=True)
 
+    # Default unicode string representation
+    # of an instance.
     def __unicode__(self):
         return "%s - %s" % (self.title, self.youtube_id)
 
+    # Returns the youtube embed url.
     def youtube_url(self):
         return 'https://youtube.com/embed/{}'.format(self.youtube_id)
 
+    # Returns the url to view the single video.
     def get_absolute_url(self):
         from django.core.urlresolvers import reverse
         return reverse('trickfeed.views.view_video', args=[str(self.id)])
+
+    # Method to detect whether, the video
+    # was added within the past 24 hours. Should
+    # return true or false.
+    def is_this_new(self):
+        pass
 
     class Meta:
         ordering = ['-added']
