f114654bfa9dc6c5b45864ab536025756fb7c304
Tuss4 <tuss4dbfn@gmail.com>
2014-06-15 20:28:02 -0500

    I've been wrestling with the tastypie manytomany manager for patch updatinga tricker's favorites. Going to go for a ducttape/custom view solution. Should be faster for now for the seek of meeting the milestone, will add many to many issue to backlog.

--- trickfeed/static/view_video.js
+++ trickfeed/static/view_video.js
@@ -21,8 +21,30 @@ var TheatreApp = function(player) {
 
 //User favorites controls
 //Avoid hardcoding any attributes
-var FavoriteApp = function(user, video) {
+var FavoriteApp = function(user, video, v_array) {
+	var user_uri = user;
+	var video_uri = video;
+	var v_array = v_array;
+	v_array.push(video_uri);
+
 	var addButton = $('.button-add-to-favs');
 	var removeButton = $('.button-remove-from-favs');
 	var flagButton = $('.button-not-tricking');
+
+	//Events
+	addButton.click(function() {
+		$.ajax({
+			url: user_uri,
+			data: {"favorites": v_array},
+			dataType: "json",
+			contentType: "application/json",
+			type: "PUT",
+			success: function(){
+				alert("Biscuits and gravy!");
+			},
+			error: function(response) {
+				console.log(response.responseText);
+			}
+		});
+	});
 };
